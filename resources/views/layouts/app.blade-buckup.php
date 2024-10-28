dc65721b626c833bad5d772fd8d19482bc0402f4
pravedens <107057525+pravedens@users.noreply.github.com>
2024-10-28 11:32:14 +0500

    Установка шаблона Sidebar

--- resources/views/layouts/app.blade-buckup.php
+++ resources/views/layouts/app.blade-buckup.php
@@ -16,7 +16,7 @@
     </head>
     <body class="font-sans antialiased">
         <div class="min-h-screen bg-gray-100 dark:bg-gray-900">
-            @include('layouts.sidebar')
+            @include('layouts.navigation')
 
             <!-- Page Heading -->
             @isset($header)
@@ -34,3 +34,4 @@
         </div>
     </body>
 </html>
+

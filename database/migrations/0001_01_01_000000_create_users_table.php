0a439519aed2b4f1146b064b756338c380ac4c81
pravedens <107057525+pravedens@users.noreply.github.com>
2024-10-28 10:54:36 +0500

    Установка Preline

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -17,6 +17,9 @@ return new class extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->string('photo_profile')->nullable();
+            $table->date('birthdate')->nullable();
+            $table->string('сhurch')->nullable();
             $table->rememberToken();
             $table->timestamps();
         });
